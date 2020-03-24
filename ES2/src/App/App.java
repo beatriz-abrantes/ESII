c39d1cbaee4e743de1848a0cf14572b09788145f
blaso1-iscteiul <bea.abrantes99@gmail.com>
2020-03-24 18:55:46 +0000

    testes novos

--- ES2/src/App/App.java
+++ ES2/src/App/App.java
@@ -6,6 +6,49 @@ package App;
  */
 public class App 
 {
+	
+	/** The number. */
+	private static int number = 1;
+	
+	/**
+	 * Gets the number.
+	 *
+	 * @return the number
+	 */
+	public static int getNumber() {
+		return number;
+	}
+	
+	/**
+	 * Sets the number.
+	 *
+	 * @param number the new number
+	 */
+	public static void setNumber(int number) {
+		App.number = number;
+	}
+	
+	/**
+	 * Change number.
+	 */
+	public static void change_number() {
+		if (impar(number)) { 
+			setNumber(2);
+		}
+	}
+
+	/**
+	 * Impar.
+	 *
+	 * @param number the number
+	 * @return true, if successful
+	 */
+	public static boolean impar(int number) {
+		if (number%2!=0) {
+			return true;
+		}
+		return false; 
+	}
     
     /**
      * The main method.
@@ -14,7 +57,13 @@ public class App
      */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	if (number==1) {
+			System.out.println( "Hello World 1!" );
+			change_number();
+		}
+		if (number==2) {
+			System.out.println( "Hello World 2!" );
+		}
     }
 }
  
--- ES2/src/Test/AppTest.java
+++ ES2/src/Test/AppTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import App.App;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -39,5 +40,44 @@ public class AppTest
     {
         assertTrue( true );
     }
-   
+    
+    /**
+     * Test.
+     */
+    @org.junit.Test
+    public void test() {
+    	App a = new App();
+    	assertEquals(1, a.getNumber());
+    }
+    
+    /**
+     * Test 1.
+     */
+    @org.junit.Test
+    public void test1() {
+    	App a = new App();
+    	int number = 5;
+    	assertTrue(a.impar(number));
+    }
+    
+    /**
+     * Test 2.
+     */
+    @org.junit.Test
+    public void test2() {
+    	App a = new App();
+    	a.change_number();
+    	int number = 2;
+    	assertEquals(2, a.getNumber());
+    }
+    
+    /**
+     * Test 3.
+     */
+    @org.junit.Test
+    public void test3() {
+    	App a = new App();
+    	int number = 4;
+    	assertFalse(a.impar(number));
+    }
 }
