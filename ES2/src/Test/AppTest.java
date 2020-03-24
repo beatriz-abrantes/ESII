4bba19a51ad4c7ffb0f4711db164d37f6896cb6c
blaso1-iscteiul <bea.abrantes99@gmail.com>
2020-03-24 00:28:51 +0000

    .

--- ES2/src/Test/AppTest.java
+++ ES2/src/Test/AppTest.java
@@ -3,10 +3,10 @@ package Test;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-/**
+ 
+/** 
  * Unit test for simple App.
- */
+ */ 
 public class AppTest 
     extends TestCase
 {
