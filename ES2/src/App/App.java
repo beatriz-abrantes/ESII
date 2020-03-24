1021525f7ef2c6299a11550bbd27ef9a85eee3fd
blaso1-iscteiul <bea.abrantes99@gmail.com>
2020-03-24 13:05:26 +0000

    jdoc

--- ES2/src/App/App.java
+++ ES2/src/App/App.java
@@ -1,11 +1,17 @@
 package App;
 
+// TODO: Auto-generated Javadoc
 /**
- * Hello world!
- *
+ * Hello world!.
  */
 public class App 
 {
+    
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
